5799d9431dbef37c6a786f8bb9fa8425410e5abb
Robert Lee <bobbydwise1@hotmail.com>
2018-12-14 13:10:10 -0800

    Redo controllers.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,82 +1,46 @@
-require('pg')
-require('pry')
-
-class Product
-  attr_accessor :name, :cost, :origin, :id
-
-  def initialize(attributes)
-    @name = attributes.fetch(:name)
-    @cost = attributes.fetch(:cost).to_f
-    @origin = attributes.fetch(:origin)
-    @id = attributes.fetch(:id).to_i
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
   end
 
-  def ==(other)
-    same_class = self.class().eql?(other.class())
-    same_name = self.name().eql?(other.name())
-    same_class.&(same_name)
+  def show
+    @product = Product.find(params[:id])
   end
 
-  def self.all
-    returned_proudcts = DB.exec("SELECT * FROM proudcts ORDER BY id")
-    proudcts = []
-    returned_proudcts.each do |each|
-      name = each.fetch("name")
-      cost = each.fetch("cost").to_f
-      origin = each.fetch("origin")
-      id = each.fetch("id").to_i()
-      proudcts.push(Product.new({:name => name, :cost => cost, :origin => origin, :id => id}))
-    end
-    return proudcts
+  def new
+    @product = Product.new
   end
 
-  def save
-    result = DB.exec("INSERT INTO proudcts (name, cost, origin) VALUES ('#{@name}', #{@cost}, '#{@origin}') RETURNING id")
-    @id = result.first().fetch("id").to_i
-  end
-
-  def self.find(id)
-    returned_proudcts = DB.exec("SELECT * FROM proudcts WHERE id = #{id}")
-    returned_prouducts.each() do |product|
-      name = product.fetch("name")
-      cost = product.fetch("cost").to_f
-      origin = product.fetch("origin")
-      id = proudct.fetch("id").to_i
-      return Product.new({:name => name, :id => id})
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product successfully added!"
+      redirect_to products_path
+    else
+      render :new
     end
   end
 
-  def reviews
-    returned_reviews = DB.exec("SELECT * FROM reviews WHERE proudct_id = #{self.id}")
-    review_array = []
-    returned_reviews.each() do |review|
-      author = review.fetch("author")
-      content_body = review.fetch("content_body")
-      rating = review.fetch("rating")
-      proudct_id = review.fetch("proudct_id")
-      id = review.fetch("id").to_i()
-      review_array.push(Review.new({:author => name, :content_body => content_body, :rating => rating, :proudct_id => proudct_id, :id => id}))
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
     end
-    return review_array
   end
 
-  def update(attributes)
-    new_name = attributes.fetch(:name)
-    new_cost = attributes.fetch(:cost)
-    new_origin = attributes.fetch(:origin)
-    found_id = DB.exec("UPDATE proudcts SET name = '#{new_name}' WHERE id = #{@id} RETURNING id")
-    if found_id
-      @name = new_name
-      @cost = new_cost
-      @origin = new_origin
-      return true
-    end
-    return false
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
-
-  def delete
-    DB.exec("DELETE FROM proudcts WHERE id = #{self.id}")
-    # DB.exec("DELETE FROM volunteers WHERE proudct_id = #{@self.id}")
+private
+  def product_params
+    params.require(:product).permit(:name)
   end
-
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,78 +1,44 @@
-require('pg')
-require('pry')
-
-class Review
-  attr_accessor :author, :content_body, :rating, :product_id, :id
-
-  def initialize(attributes)
-    @author = attributes.fetch(:author)
-    @content_body = attributes.fetch(:content_body)
-    @rating = attributes.fetch(:rating)
-    @product_id = attributes.fetch(:product_id)
-    @id = attributes.fetch(:id)
+class ReviewsController < ApplicationController
+  def new
+    @proudct = Product.find(params[:proudct_id])
+    @review = @proudct.reviews.new
   end
 
-  def ==(other)
-    same_class = self.class().eql?(other.class())
-    same_author = self.author().eql?(other.author())
-    same_class.&(same_author)
-  end
-
-  def self.all
-    returned_reviews = DB.exec("SELECT * FROM reviews ORDER BY id")
-    reviews = []
-    returned_reviews.each do |each|
-      author = each.fetch("author")
-      content_body = each.fetch("content_body")
-      rating = each.fetch("rating").to_i
-      product_id = each.fetch("product_id").to_i
-      id = each.fetch("id").to_i()
-      reviews.push(Review.new({:author => author, :product_id => product_id, :id => id}))
-    end
-    return reviews
-  end
-
-  def save
-    result = DB.exec("INSERT INTO reviews (author, content_body, rating, product_id) VALUES ('#{@author}', '#{@content_body}', #{@rating}, '#{@product_id}') RETURNING id;")
-    @id = result.first().fetch("id").to_i()
-  end
-
-  def self.find(id)
-    returned_reviews = DB.exec("SELECT * FROM reviews WHERE id = #{id};")
-    returned_reviews.each() do |review|
-      author = review.fetch("author")
-      content_body = review.fetch("content_body")
-      rating = review.fetch("rating").to_i
-      product_id = review.fetch("product_id").to_i
-      id = review.fetch("id").to_i()
-      return Review.new({:author => author, :product_id => product_id, :id => id})
+  def create
+    @proudct = Product.find(params[:proudct_id])
+    @review = @proudct.reviews.new(review_params)
+    if @review.save
+      redirect_to proudct_path(@review.proudct)
+    else
+      render :new
     end
   end
 
-  def update(attributes)
-    new_author = attributes.fetch(:author)
-    new_content_body = attributes.fetch(:content_body)
-    new_rating = attributes.fetch(:rating).to_i
-    found_id = DB.exec("UPDATE reviews SET author = '#{new_author}' WHERE id = #{@id} RETURNING id")
-    if found_id
-      @author = new_author
-      return @author
+  def edit
+    @proudct = Product.find(params[:proudct_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @proudct = Product.find(params[:proudct_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to proudct_path(@review.proudct)
+    else
+      render :edit
     end
-    return false
   end
 
-  def assign_product_id(attributes)
-    new_product_id = attributes.fetch(:product_id)
-    found_id = DB.exec("UPDATE reviews SET product_id = #{new_product_id} WHERE id = #{@id} RETURNING id")
-    if found_id
-      @product_id = new_product_id
-      return @product_id
-    end
-    return false
+  def destroy
+    @proudct = Product.find(params[:proudct_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to proudct_path(@review.proudct)
   end
+private
+  def review_params
+    params.require(:review).permit(:description, :done)
 
-  def delete
-    DB.exec("DELETE FROM reviews WHERE id = #{self.id}")
   end
-
 end
